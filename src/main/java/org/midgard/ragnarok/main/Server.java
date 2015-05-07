082d95e234cf92cc2019ad34e4fad1b01e753047
Vite Falcon <vitefalcon@gmail.com>
2015-05-07 06:14:54 -0700

    Modified application to load the appropriate Spring configurations based on the server type.

--- src/main/java/org/midgard/ragnarok/main/Server.java
+++ src/main/java/org/midgard/ragnarok/main/Server.java
@@ -1,8 +1,10 @@
-package org.midgard.ragnarok;
+package org.midgard.ragnarok.main;
 
+import com.google.common.collect.ImmutableMap;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.ParseException;
 import org.midgard.ragnarok.app.CliArgs;
+import org.midgard.ragnarok.app.CommandLineArgsFactory;
 import org.midgard.ragnarok.data.ServerType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -15,6 +17,7 @@ import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.util.Map;
 import java.util.Optional;
 
 /**
@@ -22,9 +25,21 @@ import java.util.Optional;
  */
 @Service
 public class Server {
-    private static final String[] CONFIG_LOCATIONS = new String[]{
+    private static final String[] LOGIN_SERVER_CONFIG_LOCATIONS = new String[]{
             "spring/login-server.xml"
     };
+    private static final String[] CHAR_SERVER_CONFIG_LOCATIONS = new String[]{
+            "spring/login-server.xml"
+    };
+    private static final String[] MAP_SERVER_CONFIG_LOCATIONS = new String[]{
+            "spring/login-server.xml"
+    };
+    private static final Map<ServerType, String[]> SERVER_CONFIGS = ImmutableMap
+            .<ServerType, String[]>builder()
+            .put(ServerType.LOGIN, LOGIN_SERVER_CONFIG_LOCATIONS)
+            .put(ServerType.CHARACTER, CHAR_SERVER_CONFIG_LOCATIONS)
+            .put(ServerType.MAP, MAP_SERVER_CONFIG_LOCATIONS)
+            .build();
 
     private final CommandLine commandLine;
     private final String serverName;
@@ -51,10 +66,10 @@ public class Server {
     }
 
     private Optional<ServerType> getServerType() {
-        return Optional.ofNullable(ServerType.valueOf(commandLine.getOptionValue("s")));
+        return retrieveServerType(commandLine);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
         BeanDefinition argsBean = BeanDefinitionBuilder
                 .rootBeanDefinition(CliArgs.class)
@@ -64,8 +79,26 @@ public class Server {
         GenericApplicationContext cmdArgCtx = new GenericApplicationContext(beanFactory);
         // Must call refresh to initialize context
         cmdArgCtx.refresh();
+        // Retrieve server-type and the corresponding Spring XML configurations for it.
+        ServerType serverType = getServerType(args);
+        String[] configLocations = SERVER_CONFIGS.get(serverType);
         // Create application context, passing command line context as parent
-        AbstractApplicationContext context = new ClassPathXmlApplicationContext(CONFIG_LOCATIONS, cmdArgCtx);
+        AbstractApplicationContext context = new ClassPathXmlApplicationContext(configLocations, cmdArgCtx);
         context.registerShutdownHook();
+    }
+
+    private static ServerType getServerType(String[] args) throws ParseException {
+        CliArgs cliArgs = new CliArgs(args);
+        ServerCliOptions options = new ServerCliOptions();
+        CommandLine cli = new CommandLineArgsFactory(options, cliArgs).parseCommandLine();
+        Optional<ServerType> serverType = retrieveServerType(cli);
+        if (serverType.isPresent()) {
+            return serverType.get();
+        }
+        throw new ParseException("Failed to find server-type from CLI arguments provided!");
+    }
+
+    private static Optional<ServerType> retrieveServerType(CommandLine commandLine) {
+        return Optional.ofNullable(ServerType.valueOf(commandLine.getOptionValue("s")));
     }
 }
---
+++ src/main/java/org/midgard/ragnarok/main/ServerCliOptions.java
@@ -0,0 +1,9 @@
+package org.midgard.ragnarok.main;
+
+import org.midgard.ragnarok.app.AbstractCliOptions;
+
+/**
+ * Created by ullatil on 07/05/2015.
+ */
+public class ServerCliOptions extends AbstractCliOptions {
+}
